2b566ca5901fb34d40a6af3d07d3b65fbf0ddd97
bridgidburkert <63748187+bridgidburkert@users.noreply.github.com>
2020-06-19 14:08:22 -0400

    Update MainTabs.tsx

--- src/pages/MainTabs.tsx
+++ src/pages/MainTabs.tsx
@@ -1,7 +1,7 @@
 import React  from 'react';
 import { IonTabs, IonRouterOutlet, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/react';
 import { Route, Redirect } from 'react-router';
-import { calendar, location, informationCircle, people } from 'ionicons/icons';
+import { personOutline, star, location, informationCircle, gameController } from 'ionicons/icons';
 import SchedulePage from './SchedulePage';
 import SpeakerList from './SpeakerList';
 import SpeakerDetail from './SpeakerDetail';
@@ -16,7 +16,7 @@ const MainTabs: React.FC<MainTabsProps> = () => {
   return (
     <IonTabs>
       <IonRouterOutlet>
-        <Redirect exact path="/tabs" to="/tabs/schedule" />
+        <Redirect exact path="/tabs" to="/tabs/about" />
         {/*
           Using the render method prop cuts down the number of renders your components will have due to route changes.
           Use the component prop when your component depends on the RouterComponentProps passed in automatically.
@@ -31,20 +31,20 @@ const MainTabs: React.FC<MainTabsProps> = () => {
       </IonRouterOutlet>
       <IonTabBar slot="bottom">
         <IonTabButton tab="schedule" href="/tabs/schedule">
-          <IonIcon icon={calendar} />
-          <IonLabel>Schedule</IonLabel>
+          <IonIcon icon={personOutline} />
+          <IonLabel>Profile</IonLabel>
         </IonTabButton>
         <IonTabButton tab="speakers" href="/tabs/speakers">
-          <IonIcon icon={people} />
-          <IonLabel>Speakers</IonLabel>
-        </IonTabButton>
+          <IonIcon icon={star} />
+          <IonLabel>Favorites</IonLabel>
+       </IonTabButton>
         <IonTabButton tab="map" href="/tabs/map">
           <IonIcon icon={location} />
-          <IonLabel>Map</IonLabel>
-        </IonTabButton>
+          <IonLabel>Search Nearby</IonLabel>
+          </IonTabButton>
         <IonTabButton tab="about" href="/tabs/about">
-          <IonIcon icon={informationCircle} />
-          <IonLabel>About</IonLabel>
+          <IonIcon icon={gameController} />
+          <IonLabel>Spinner Game</IonLabel>
         </IonTabButton>
       </IonTabBar>
     </IonTabs>
